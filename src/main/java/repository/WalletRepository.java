89e98516c730180294e2439ab307623228917f38
NoutVivo <fst8822@gmail.com>
2025-06-27 00:04:59 +0300

    refactor WalletRepository

--- src/main/java/repository/WalletRepository.java
+++ src/main/java/repository/WalletRepository.java
@@ -2,10 +2,16 @@ package repository;
 
 import domain.entity.WalletEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface WalletRepository extends JpaRepository<UUID, WalletEntity> {
+
+    @Query("select we from WalletEntity we where we.id =:id")
+    Optional<WalletEntity> findById(@Param("id") UUID id);
 }
