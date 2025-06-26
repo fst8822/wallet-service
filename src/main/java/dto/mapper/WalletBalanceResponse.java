476950f157c6d1a3bd709b7fd3d1f9e761933a63
NoutVivo <fst8822@gmail.com>
2025-06-27 00:04:48 +0300

    refactor WalletBalanceResponse

--- src/main/java/dto/mapper/WalletBalanceResponse.java
+++ src/main/java/dto/mapper/WalletBalanceResponse.java
@@ -1,8 +1,13 @@
 package dto.mapper;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 public record WalletBalanceResponse(
-        BigDecimal amount
+
+        UUID id,
+        BigDecimal balance,
+        LocalDateTime createdAt
 ) {
 }
