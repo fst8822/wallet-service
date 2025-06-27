0efb05f139c1111787fef6deb9b82b8bb5b6ad77
NoutVivo <fst8822@gmail.com>
2025-06-27 14:43:36 +0300

    refactor WalletRepository

--- src/main/java/repository/WalletRepository.java
+++ src/main/java/repository/WalletRepository.java
@@ -1,7 +1,9 @@
 package repository;
 
 import domain.entity.WalletEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -12,6 +14,7 @@ import java.util.UUID;
 @Repository
 public interface WalletRepository extends JpaRepository<WalletEntity, UUID> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select we from WalletEntity we where we.id =:id")
     Optional<WalletEntity> findById(@Param("id") UUID id);
 }
