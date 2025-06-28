3c7cf535f38eaf55f15efb0ad53e58162f6757ec
NoutVivo <fst8822@gmail.com>
2025-06-28 23:24:40 +0300

    add WalletServiceTest

---
+++ src/test/java/org/das/wallet/service/WalletServiceTest.java
@@ -0,0 +1,24 @@
+package org.das.wallet.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class WalletServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void processOperation() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
