576382b8d5d2be6be39a74ee2775dfe736490b6b
NoutVivo <fst8822@gmail.com>
2025-06-29 01:01:32 +0300

    add test

---
+++ src/test/java/org/das/wallet/controller/WalletControllerTest.java
@@ -0,0 +1,21 @@
+package org.das.wallet.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WalletControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void processOperation() {
+    }
+
+    @Test
+    void getWalletBalance() {
+    }
+}
