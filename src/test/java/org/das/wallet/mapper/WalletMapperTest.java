a63eac05106f97b948952880f9b181df2f8794a8
NoutVivo <fst8822@gmail.com>
2025-06-29 01:17:28 +0300

    add test class WalletMapperTest

---
+++ src/test/java/org/das/wallet/mapper/WalletMapperTest.java
@@ -0,0 +1,21 @@
+package org.das.wallet.mapper;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WalletMapperTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void shouldGetEntityToDomain() {
+    }
+
+    @Test
+    void shouldGetDomainToEntity() {
+    }
+}
