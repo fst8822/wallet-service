6e8cb4d42f00f70a56089c36e12e96055c6b5fdc
NoutVivo <fst8822@gmail.com>
2025-06-29 09:03:40 +0300

    add return type in WalletService

--- src/main/java/org/das/wallet/service/WalletService.java
+++ src/main/java/org/das/wallet/service/WalletService.java
@@ -7,6 +7,6 @@ import java.util.UUID;
 
 public interface WalletService {
 
-    void processOperation(WalletOperationRequest request);
+    Wallet processOperation(WalletOperationRequest request);
     Wallet findById(UUID id);
 }
