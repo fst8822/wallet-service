a7bd67ac72f08dbfe7fca313c96baafc0390ed45
NoutVivo <fst8822@gmail.com>
2025-06-29 09:04:09 +0300

    refactor WalletMapperTest

--- src/test/java/org/das/wallet/mapper/WalletMapperTest.java
+++ src/test/java/org/das/wallet/mapper/WalletMapperTest.java
@@ -1,21 +1,54 @@
 package org.das.wallet.mapper;
 
+import org.das.wallet.domain.Wallet;
+import org.das.wallet.entity.WalletEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WalletMapperTest {
 
+    private WalletMapper walletMapper;
+    private UUID walletId;
+    private BigDecimal balance;
+
     @BeforeEach
     void setUp() {
+        walletMapper = new WalletMapper();
+        walletId = UUID.randomUUID();
+        balance = new BigDecimal("1000.00");
+    }
+
+
+    @Test
+    void shouldConvertEntityToDomainGetDomain() {
+        WalletEntity entity = new WalletEntity(walletId, balance);
+        Wallet domain = walletMapper.entityToDomain(entity);
+        assertNotNull(domain);
+        assertEquals(entity.getId(), domain.id());
+        assertEquals(entity.getBalance(), domain.balance());
     }
 
     @Test
-    void shouldGetEntityToDomain() {
+    void shouldConvertDomainToEntityGetEntity() {
+        Wallet domain = new Wallet(walletId, balance);
+        WalletEntity entity = walletMapper.domainToEntity(domain);
+        assertNotNull(domain);
+        assertEquals(domain.id(), entity.getId());
+        assertEquals(domain.balance(), entity.getBalance());
     }
 
     @Test
-    void shouldGetDomainToEntity() {
+    void shouldEntityNullWhenEntityToDomain_ThrowsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> walletMapper.entityToDomain(null));
+    }
+
+    @Test
+    void shouldEntityNullWhenDomainToEntity_ThrowsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> walletMapper.domainToEntity(null));
     }
 }
