916eb536a7fd9a590544f8419494efd6623235c2
NoutVivo <fst8822@gmail.com>
2025-06-29 15:07:34 +0300

    create WalletRepositoryTest

---
+++ src/test/java/org/das/wallet/repository/WalletRepositoryTest.java
@@ -0,0 +1,17 @@
+package org.das.wallet.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WalletRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByIdWithLock() {
+    }
+}
